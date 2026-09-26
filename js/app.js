6da83f7db38039b463bb3841788fe50df10b274c
abbahaapp <ali.mohammad.karami@outlook.com>
2026-09-27 02:39:34 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3229,10 +3229,9 @@ async function generateImage() {
                 'image-export-clone'
             );
 
-            imageClone.style.position = 'fixed';
-            imageClone.style.left = '0';
+            imageClone.style.position = 'absolute';
+            imageClone.style.left = '-100000px';
             imageClone.style.top = '0';
-            imageClone.style.visibility = 'hidden';
 
             imageClone.style.width =
                 `${renderedWidth}px`;
